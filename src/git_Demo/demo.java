3f25a4923485b719b4a76b052ad48b769b8c1d26
indirachakka0702 <Indira1234@>
2020-05-07 17:02:24 -0600

    commiting first time

---
+++ src/git_Demo/demo.java
@@ -0,0 +1,24 @@
+package git_Demo;
+
+public class demo {
+
+	public static void main(String[] args) {
+		System.out.println("I am learning git today");
+		// GIT HUB learning
+		// Git commands add, commit, push
+		// Git status, checkout
+		
+   // commands fir pushing the projects to git hub
+		// create repository in git hub
+		// came to project work spaces then copy the project path
+		//now open command prompt --> "cd paste the project path"
+		// "git init" --> to initialize the project
+		// "git remote add origin <URL of github repository>" --> linking the project to github
+		// "git status"
+		// "git add ."
+		// "git commit -m "comments" "
+		// "git push --set-upstream origin master"
+		
+	}
+
+}
